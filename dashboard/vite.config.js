d545722ae83ea5c6b9552ccdf617961159ec9283
makhmur12 <150470442+makhmur12@users.noreply.github.com>
2025-07-01 07:19:39 +0300

    Update vite.config.js

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-  host: true,
-  port: process.env.PORT || 4173
+    port: process.env.PORT || 4173,
+    host: true,
+    allowedHosts: ['zerodhclone-7.onrender.com']
+  }
 }
 })
